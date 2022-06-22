76a49a13ebbec3883e15cdf2af3060ef0e8d120d
Faisal Maarij <maarij_faisal@hotmail.com>
2022-06-22 09:14:02 +0200

    test

---
+++ exercice1/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        Bonjour à tous
+      </div>
+    );
+  }
+}
+export default App;
---
+++ exercice2/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render(){
+    let date = "21/06/2022";
+    return (
+      <div>
+      <div>
+        Bonjour à tous {date}
+      </div>
+      </div>
+    );
+  }
+}
+export default App;
